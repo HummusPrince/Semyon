a900a4d980e6f37ade20ede5ff3b58f7123275a7
HummusPrince <>
2019-10-09 03:38:13 +0300

    Jumptable for lighting correct LEDs.

--- semyon.asm
+++ semyon.asm
@@ -4,6 +4,11 @@ LEDLEN = 5
 LEDLOC = 0x300
 PCON = 0x97
 
+RLED = P3.5
+YLED = P3.4
+GLED = P3.2
+BLED = P3.3
+
 .area INTV (ABS)
 .org 0x0000
 _int_reset:
@@ -14,59 +19,61 @@ _int_reset:
 
 main:
 	mov r0, #LEDLEN
-	mov DPH, #(LEDLOC >> 8)
-	mov DPL, #(LEDLOC & 0xff)
 main_loop:
 	mov r1, #0x04
 	mov a, r0
-	movc a, @a+DPTR
+	mov dptr, #LEDLOC
+	movc a, @a+dptr
 	mov r2, a
 main_loop2:
-	;red = P3.5
-	;yellow = P3.4
-	;green = P3.2
-	;blue = P3.3
 	mov a, r2
-	;add a, 0x01
 	anl a, #0x03
-blue:
-	cjne a, #0x03, green
-	;clr P3.2
-	clr P3.3
-	acall delay
-	setb P3.3
-	sjmp read_next_led
-green:
-	cjne a, #0x02, yellow
-	clr P3.2
-	acall delay
-	setb P3.2
-	sjmp read_next_led
-yellow:
-	cjne a, #0x01, red
-	clr P3.4
-	acall delay
-	setb P3.4
-	sjmp read_next_led
-red:
-	clr P3.5
-	acall delay
-	setb P3.5
-	
+	mov r3, a
+	lcall display_led_jumptable
 read_next_led:
-	;acall delay
 	mov a, r2
 	rr a
 	rr a
 	mov r2, a
-	;cpl P3.4
 	djnz r1, main_loop2
 	djnz r0, main_loop
 	sjmp main
 
+display_led_jumptable:
+	mov dptr, #jumptable
+	mov a, r3
+	rl a 	;YOU BASTARD!!!!!!!
+	jmp @a+dptr
+jumptable:
+	sjmp light_rled
+	sjmp light_yled
+	sjmp light_gled
+	sjmp light_bled
+	
+	light_rled:
+		clr RLED
+		acall delay
+		setb RLED
+		ret
+	light_yled:
+		clr YLED
+		acall delay
+		setb YLED
+		ret
+	light_gled:
+		clr GLED
+		acall delay
+		setb GLED
+		ret
+	light_bled:
+		clr BLED
+		acall delay
+		setb BLED
+		ret
+
+
 
 delay:
-	;cpl P3.5
 	mov r5, #0x18
 	mov r6, #0x00
 	mov r7, #0x00
